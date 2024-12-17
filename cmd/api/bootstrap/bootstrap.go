2e94f54e583088d9c837afc34050254ee38a3144
Rafael Matito <rafa.matito@gmail.com>
2024-12-17 18:02:23 +0100

    feat: add environment variables

--- cmd/api/bootstrap/bootstrap.go
+++ cmd/api/bootstrap/bootstrap.go
@@ -2,13 +2,12 @@ package bootstrap
 
 import (
 	"github.com/m4t1t0/go-hotels-proxy/internal/platform/server"
-)
-
-const (
-	port = 3000
+	"os"
+	"strconv"
 )
 
 func Run() error {
-	srv := server.New(port)
+	port, _ := strconv.Atoi(os.Getenv("HTTP_PORT"))
+	srv := server.New(uint(port))
 	return srv.Run()
 }
