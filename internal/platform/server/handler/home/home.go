e9a45c412b91761b339a0b6df7ddd90bf0e6f199
Rafael Matito <rafa.matito@gmail.com>
2024-12-29 19:37:51 +0100

    feat: Install pgx and fetch information from DB

--- internal/platform/server/handler/home/home.go
+++ internal/platform/server/handler/home/home.go
@@ -1,11 +1,52 @@
 package home
 
 import (
+	"context"
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/jackc/pgx/v5"
+	"os"
 )
 
 func Handler() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return c.SendString("Ola ke ase?, eta en el handler o ke ase?")
+		connStr := fmt.Sprintf(
+			"postgres://%s:%s@%s:5432/%s",
+			os.Getenv("POSTGRESQL_READS_USERNAME"),
+			os.Getenv("POSTGRESQL_READS_PASSWORD"),
+			os.Getenv("POSTGRESQL_READS_HOST"),
+			os.Getenv("POSTGRESQL_READS_DBNAME"),
+		)
+
+		conn, err := pgx.Connect(context.Background(), connStr)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+			os.Exit(1)
+		}
+
+		defer conn.Close(context.Background())
+
+		type Accommodation struct {
+			Id       string
+			Name     string
+			Category int
+		}
+
+		query := "SELECT id, name, category FROM main_reads.all_accommodations aa LIMIT 10"
+		rows, _ := conn.Query(context.Background(), query)
+
+		var accommodations []Accommodation
+
+		for rows.Next() {
+			var acc Accommodation
+			err := rows.Scan(&acc.Id, &acc.Name, &acc.Category)
+			if err != nil {
+				return c.SendString(fmt.Sprintf("Error Fetching Accommodation Details. Error: %s", err))
+			}
+			accommodations = append(accommodations, acc)
+		}
+
+		return c.SendString(fmt.Sprintf("Accommodations\n\n%v", accommodations))
 	}
 }
